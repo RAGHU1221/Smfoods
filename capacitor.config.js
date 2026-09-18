2b7df3de44537143599c6eedbfd741df849899db
RAGHU1221 <65601879+RAGHU1221@users.noreply.github.com>
2026-09-18 17:43:18 +0530

    Add capacitor.config.js for app configuration

---
+++ capacitor.config.js
@@ -0,0 +1,11 @@
+const config = {
+  appId: 'com.srimuruganfoods.pos',
+  appName: 'Sri Murugan Foods POS',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  android: {
+    backgroundColor: '#b91c1c'
+  }
+};
+
+module.exports = config;
